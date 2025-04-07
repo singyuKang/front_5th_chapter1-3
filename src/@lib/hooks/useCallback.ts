b75a0765b87f2c738af3350d8d21f73f3c891438
singyuKang <rkdtlscks123@naver.com>
2025-04-07 20:28:50 +0900

    feat : useCallback 기능 추가

--- src/@lib/hooks/useCallback.ts
+++ src/@lib/hooks/useCallback.ts
@@ -1,10 +1,26 @@
 /* eslint-disable @typescript-eslint/no-unused-vars,@typescript-eslint/no-unsafe-function-type */
 import { DependencyList } from "react";
+import { useRef } from "./useRef";
+import { shallowEquals } from "../equalities";
+
+// const memoizedCallback = useCallback(() => mockCallback(42), deps);
 
 export function useCallback<T extends Function>(
   factory: T,
-  _deps: DependencyList,
+  _deps: DependencyList
 ) {
-  // 직접 작성한 useMemo를 통해서 만들어보세요.
-  return factory as T;
+  // 1. 이전 의존성 저장할 ref
+  const prevDependency = useRef<DependencyList | null>(null);
+  const result = useRef<Function | null>(null);
+  //2 현재 의존성과 이전 의존성 비교
+  const isChanged =
+    !prevDependency.current || !shallowEquals(_deps, prevDependency.current);
+
+  // 3. 의존성이 변경된 경우 이전의존성 업데이트 + 새로운 함수 저장
+  if (isChanged) {
+    prevDependency.current = _deps;
+    result.current = factory;
+  }
+
+  return result.current;
 }
